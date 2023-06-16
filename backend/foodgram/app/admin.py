be8d77cf8f7f830cd84c0e2538f34ca4320f6dae
Стас Басов <stas.basov@whatever.com>
2023-06-16 19:02:01 +0300

    Создание 12

--- backend/foodgram/app/admin.py
+++ backend/foodgram/app/admin.py
@@ -1,21 +1,20 @@
 from django.contrib import admin
- 
 from .models import (Follow, Tag, Ingredient, Recipe, IngredientToRecipe,
                      Favorite, ShoppingCart)
- 
- 
+
+
 class IngredientInline(admin.TabularInline):
     model = IngredientToRecipe
     extra = 3
- 
- 
+
+
 class RecipeAdmin(admin.ModelAdmin):
     list_display = ('author', 'name', 'cooking_time')
     search_fields = ('author__username', 'author__email', 'name')
     list_filter = ['tags',]
     inlines = (IngredientInline,)
- 
- 
+
+
 class FollowAdmin(admin.ModelAdmin):
     list_display = (
         'user',
@@ -23,8 +22,8 @@ class FollowAdmin(admin.ModelAdmin):
     )
     search_fields = ('user__username', 'user__email')
     empty_value_display = '-пусто-'
- 
- 
+
+
 class TegAdmin(admin.ModelAdmin):
     list_display = (
         'name',
@@ -32,18 +31,18 @@ class TegAdmin(admin.ModelAdmin):
         'slug',
     )
     empty_value_display = '-пусто-'
- 
- 
+
+
 class IngredientAdmin(admin.ModelAdmin):
     list_display = (
         'name',
         'measurement_unit',
     )
     search_fields = ('name',)
-    list_filter = ['measurement_unit',]
+    list_filter = ['measurement_unit', ]
     empty_value_display = '-пусто-'
- 
- 
+
+
 class IngredientToRecipeAdmin(admin.ModelAdmin):
     list_display = (
         'ingredient',
@@ -51,10 +50,10 @@ class IngredientToRecipeAdmin(admin.ModelAdmin):
         'amount',
     )
     search_fields = ['ingredient__name', 'recipe__name']
-    list_filter = ['recipe__tags',]
+    list_filter = ['recipe__tags', ]
     empty_value_display = '-пусто-'
- 
- 
+
+
 class FavoriteAdmin(admin.ModelAdmin):
     list_display = (
         'user',
@@ -63,18 +62,18 @@ class FavoriteAdmin(admin.ModelAdmin):
     list_filter = ('recipe__tags__name',)
     search_fields = ['user__email', 'user__username', 'recipe__name']
     empty_value_display = '-пусто-'
- 
- 
+
+
 class ShoppingCartAdmin(admin.ModelAdmin):
     list_display = (
         'user',
         'recipe',
     )
     search_fields = ['user__email', 'user__username', 'recipe__name']
-    list_filter = ['recipe__tags',]
+    list_filter = ['recipe__tags', ]
     empty_value_display = '-пусто-'
- 
- 
+
+
 admin.site.register(Follow, FollowAdmin)
 admin.site.register(Tag, TegAdmin)
 admin.site.register(Ingredient, IngredientAdmin)
