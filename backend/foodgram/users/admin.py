9f44cea255f0e1df9368825f7583cdbaaae5ddcd
Стас Басов <stas.basov@whatever.com>
2025-03-13 15:14:29 +0300

    Комментарий, описывающий изменения

--- backend/foodgram/users/admin.py
+++ backend/foodgram/users/admin.py
@@ -1,20 +1,22 @@
 from django.contrib import admin
-from django.contrib.auth import get_user_model
 
-User = get_user_model()
+from . import models
 
 
+@admin.register(models.User)
 class UserAdmin(admin.ModelAdmin):
     list_display = (
-        'email',
-        'username',
-        'first_name',
-        'last_name',
-        'password',
+        'username', 'pk', 'email', 'password', 'first_name', 'last_name',
     )
-    search_fields = ('email', 'username')
+    list_editable = ('password', )
+    list_filter = ('username', 'email')
+    search_fields = ('username', 'email')
     empty_value_display = '-пусто-'
 
 
-admin.site.unregister(User)
-admin.site.register(User, UserAdmin)
+@admin.register(models.Subscribe)
+class SubscribeAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'user', 'author')
+    list_editable = ('user', 'author')
+    empty_value_display = '-пусто-'
+
